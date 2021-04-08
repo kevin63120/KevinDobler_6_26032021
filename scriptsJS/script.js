cf4a7af6e0852793a3ac7e3b6eca1309c6814915
kevin <kevin63s@hotmail.fr>
2021-04-08 10:56:45 +0200

    script profil

--- scriptsJS/script.js
+++ scriptsJS/script.js
@@ -1,16 +1,22 @@
 
 
 // retrieved an items in the DOM for photograph card
-const containerProfil = document.querySelector(".container_photographs_profils")
-const photographName = document.querySelector(".name");
-const photographLocation = document.querySelector('.photograph_location');
-const photographDescription = document.querySelector(".photograph_description-text");
-const photographPricePerDay = document.querySelector(".photograph_pricePerDay");
-const photographTagItems = document.querySelectorAll('.personal');
-const profilsPicture = document.querySelector('.profil_picture');
-const photographDescriptionContainer = document.querySelector(".photograph_description");
-const mainIndex = document.querySelector(".main_index");
-const linkPhotographerPage = document.querySelector(".link_photographer-page");
+/*const mainIndex = document.querySelector(".main_index");
+
+const containerProfil = document.querySelectorAll(".container_photographs_profils")
+
+const linkPhotographerPage = document.querySelectorAll(".link_photographer-page");
+const profilsPicture = document.querySelectorAll('.profil_picture');
+const photographName = document.querySelectorAll(".name");
+
+const photographDescriptionContainer = document.querySelectorAll(".photograph_description");
+const photographLocation = document.querySelectorAll('.photograph_location');
+const photographDescription = document.querySelectorAll(".photograph_description-text");
+const photographPricePerDay = document.querySelectorAll(".photograph_pricePerDay");
+
+const photographFooterProfil = document.querySelectorAll(".photograph-tag");
+const photographTagList = document.querySelectorAll(".photograph_tag-item");
+const photographTagItems = document.querySelectorAll('.personal');*/
 
 
 
@@ -60,10 +66,11 @@ function linkPage(){
     return containerProfil.appendChild(document.createElement("a")).classList.add("link_photographer-page")
 }
 
-function articlePhotograph (){
-    return mainIndex.appendChild(document.createElement("article")).classList.add("container_photographs_profils");
-    
-}
+const dataProfil = (selector , data)=>{selector.innerHTML = data}
+
+let gars = new Profils("aaaaaaa");
+
+//dataProfil(photographName,gars.name)
 
 
 fetch("/data-profils/data-photographers.json")
@@ -72,29 +79,51 @@ fetch("/data-profils/data-photographers.json")
         
         const photographers = data.photographers;
         const pictures = data.media;
+        
        console.log(pictures)
-     for ( photographer of photographers){
-          
+     for (let photographer of photographers){
+     
+        const mainIndex = document.querySelector(".main_index");
+
+        const containerProfil = document.querySelectorAll(".container_photographs_profils")
+        
+        const linkPhotographerPage = document.querySelectorAll(".link_photographer-page");
+        const profilsPicture = document.querySelectorAll('.profil_picture');
+        const photographName = document.querySelectorAll(".name");
+        
+        const photographDescriptionContainer = document.querySelectorAll(".photograph_description");
+        const photographLocation = document.querySelectorAll('.photograph_location');
+        const photographDescription = document.querySelectorAll(".photograph_description-text");
+        const photographPricePerDay = document.querySelectorAll(".photograph_pricePerDay");
+        
+        const photographFooterProfil = document.querySelectorAll(".photograph-tag");
+        const photographTagList = document.querySelectorAll(".photograph_tag-item");
+        const photographTagItems = document.querySelectorAll('.personal');
+            
+
        
     const article =  mainIndex.appendChild(document.createElement("article"));
     const a = document.createElement("a");
     const containerDescription = document.createElement("div");
     const footerProfil =document.createElement("footer");
     const list = document.createElement("ul");
+    const p = document.createElement("p")
+    
       article.classList.add("container_photograph_profils");
       article.appendChild(a).classList.add("link_photographer-page");
       article.appendChild(containerDescription).classList.add("photograph_description");
       article.appendChild(footerProfil).classList.add("photograph_tag");
       a.appendChild(document.createElement("img")).classList.add("profil_picture");
       a.appendChild(document.createElement("h2")).classList.add("name");
-      containerDescription.appendChild(document.createElement("p")).classList.add('photograph_location');
+      containerDescription.appendChild(document.createElement("p")).classList.add("photograph_location");
       containerDescription.appendChild(document.createElement("p")).classList.add('photograph_description-text');
       containerDescription.appendChild(document.createElement("p")).classList.add('photograph_pricePerDay');
       footerProfil.appendChild(list).classList.add("photograph_tag-list");
       list.appendChild(document.createElement("li")).classList.add("photograph_tag-item")
-
       
-
+      dataProfil(photographLocation,photographer.city)
+       
+      console.log(photographLocation)
        
     }
       
